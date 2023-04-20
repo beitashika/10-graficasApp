4a89f8655453cfe8b3703da90f37adc28dd9e0ef
CYC <beatrizA@cyc.es>
2023-04-20 16:06:19 +0200

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GraficasModule } from './graficas/graficas.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+    path: 'grafica',
+    loadChildren: () => import('./graficas/graficas.module').then( m => m.GraficasModule )
+  },
+  {
+    path: '**',
+    redirectTo : 'grafica'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
