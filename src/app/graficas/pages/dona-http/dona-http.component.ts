66c7e30841de00fe475f005e01698a57cc9685fa
CYC <beatrizA@cyc.es>
2023-04-21 09:25:01 +0200

    mostrar info graf donahttp

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,21 +1,37 @@
 import { Component } from '@angular/core';
 import { GraficasService } from '../../services/graficas.service';
+import { ChartData, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona-http',
   templateUrl: './dona-http.component.html'
 })
 export class DonaHttpComponent {
+  // Doughnut
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [350, 450, 100],
+        backgroundColor: ['#6405F0', '#0724E3', '#05A0F0'] //Ya no es un arreglo de Color
+      },
+    ]
+  };
 
-  constructor ( private graficasService: GraficasService){}
+  public doughnutChartType: ChartType = 'doughnut';
+  constructor(private graficasService: GraficasService) { }
 
   ngOnInit(): void {
     this.graficasService.getUsuariosRedesSociales()
-        .subscribe( data =>{
+      .subscribe(data => {
 
-            
-
-        });
+        this.doughnutChartData={
+          labels:Object.keys(data),
+          datasets:[{data:Object.values(data)}]
+        }
+       
+      });
   }
 
 
