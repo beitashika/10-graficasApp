3f38b48fa6e20ac5511ba5cf5ac049b746c4a07d
CYC <beatrizA@cyc.es>
2023-04-21 08:46:26 +0200

    añadir flexibilidad a nuestro componente personalizado

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,38 +1,84 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
- 
-import { ChartData, ChartOptions, ChartType } from 'chart.js';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
  
 
+ 
+ 
 @Component({
-  selector: 'app-grafica-barra',
-  templateUrl: './grafica-barra.component.html'
- 
+    selector: 'app-grafica-barra',
+    templateUrl: './grafica-barra.component.html',
 })
-export class GraficaBarraComponent {
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+export class GraficaBarraComponent implements OnInit {
  
-  public barChartOptions: ChartOptions = {
-    responsive: true,
-   
-  };
-
-
-  public barChartType: ChartType = 'bar';
-
-barChartData: ChartData<'bar'> = {
-  labels: [ '2020', '2021', '2022', '2023', '2024', '2025', '2026' ],
-  datasets: [
-    { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#ED5F76', hoverBackgroundColor:'red' },
-    { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#f763C4', hoverBackgroundColor:'red' },
-    { data: [ 8, 38, 70, 59, 66, 80, 100 ], label: 'Series C' , backgroundColor: '#D665E0', hoverBackgroundColor:'red'}
-  ],
-};
-
-constructor() { }
-
-ngOnInit(): void {
-}
-
-
+    @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+    @Input() horizontal: boolean = false;
+    @Input() barChartData!: ChartData<'bar'>;
+ 
+    constructor(){}
+ 
+    ngOnInit(): void {
+        if (this.horizontal){
+            //Esto muestra la gráfica en sentido horizontal y todos los datos en ese sentido.
+            this.barChartOptions!.indexAxis = 'y';
+            this.barChartOptions!.scales!["y"]!.min = 0;
+        }
+    }
+ 
+    public barChartOptions: ChartConfiguration['options'] = {
+        responsive: true,
+        scales: {
+            x: {},
+            y: {
+                min: 10
+            }
+        },
+        plugins: {
+            legend: {
+                display: true,
+            }
+           
+        }
+    }
+ 
+    public barChartType: ChartType = 'bar';
+    public barChartPlugins = [
+      //  DataLabelsPlugin
+    ];
+    
+    public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {}
+ 
+    public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {}
+ 
+    public randomize(): void {
+        // Only Change 3 values
+        this.barChartData.datasets[0].data = [
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100)];
+        this.barChartData.datasets[1].data = [
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100)
+        ];
+        this.barChartData.datasets[2].data = [
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100),
+            Math.round(Math.random() * 100)
+        ];
+ 
+        this.chart?.update();
+    }
 }
--- src/app/graficas/pages/barras-dobles/barras-dobles.component.ts
+++ src/app/graficas/pages/barras-dobles/barras-dobles.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
-
+import { ChartData } from 'chart.js';
+ 
 @Component({
-  selector: 'app-barras-dobles',
-  templateUrl: './barras-dobles.component.html'
+    selector: 'app-barras-doble',
+    templateUrl: './barras-dobles.component.html',
 })
 export class BarrasDoblesComponent {
-
+    labelsData: string[] = ['2021', '2022', '2023', '2024', '2025'];
+    
+    proveedoresData: ChartData<'bar'> = {
+        labels: this.labelsData,
+        datasets:[
+            { data: [100, 200, 300, 400, 500], label: 'Vendedor A' },
+            { data: [50, 250, 30, 450, 200], label: 'Vendedor B' }
+        ]
+    };
+ 
+    productoData: ChartData<'bar'> = {
+        labels: this.labelsData,
+        datasets:[
+            { data: [200, 300, 400, 300, 100], label: 'Carros', backgroundColor: 'blue' }
+        ]
+    };
+ 
 }
