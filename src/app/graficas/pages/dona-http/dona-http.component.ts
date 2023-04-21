955fd6fe4b26b8e8d34557dea59af124e8113955
CYC <beatrizA@cyc.es>
2023-04-21 09:09:21 +0200

    graf dona mediante peticion http

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
-  templateUrl: './dona-http.component.html',
-  styleUrls: ['./dona-http.component.css']
+  templateUrl: './dona-http.component.html'
 })
 export class DonaHttpComponent {
+
+  constructor ( private graficasService: GraficasService){}
+
+  ngOnInit(): void {
+    this.graficasService.getUsuariosRedesSociales()
+        .subscribe( data =>{
+
+            
+
+        });
+  }
+
+
+
 
 }
