8f0210c7c7b54f5721d2cd2e399bc0ea2dabe53d
CYC <beatrizA@cyc.es>
2023-04-21 08:57:18 +0200

    grafica de dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
-  templateUrl: './dona.component.html',
-  styleUrls: ['./dona.component.css']
+  templateUrl: './dona.component.html'
 })
 export class DonaComponent {
+ // Doughnut
+ public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+ public doughnutChartData: ChartData<'doughnut'> = {
+   labels: this.doughnutChartLabels,
+   datasets: [
+     { data: [ 350, 450, 100 ],
+      backgroundColor: ['#6405F0','#0724E3', '#05A0F0'] //Ya no es un arreglo de Color
+     },
+    
+    //  { data: [ 50, 150, 120 ] },
+    //  { data: [ 250, 130, 70 ] }
+   ]
+ };
 
+ public doughnutChartType: ChartType = 'doughnut';
+
+ // events
+ public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+   console.log(event, active);
+ }
+
+ public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+   console.log(event, active);
+ }
+
+ 
 }
